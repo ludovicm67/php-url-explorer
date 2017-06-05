2d93a85a1b583d9b774dfe94aabd5691fa24a4ea
ludovicm67 <ludovicmuller1@gmail.com>
2017-06-05 15:05:31 +0200

    exec a Request from the Explorer

--- Explorer.php
+++ Explorer.php
@@ -2,14 +2,19 @@
 
 namespace ludovicm67\Url\Explorer;
 
+use ludovicm67\Url\Explorer\Request\Request;
+use ludovicm67\Url\Explorer\Request\RequestBuilder;
+
 class Explorer {
 
     private $url;
     private $opts;
+    private $request;
 
     public function __construct($url, $opts = []) {
-        $this->url = $url;
-        $this->opts = $opts;
+        $this->url     = $url;
+        $this->opts    = $opts;
+        $this->request = Request::fetchAll(new RequestBuilder($url, $opts));
     }
 
     public function getUrl() {
@@ -18,6 +23,10 @@ class Explorer {
 
     public function getOpts() {
         return $this->opts;
+    }
+
+    public function getRequest() {
+        return $this->request;
     }
 
 }
