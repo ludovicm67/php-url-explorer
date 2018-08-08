6bae15fd35be9ee67af11d40667ddbbad5a516b4
ludovicm67 <ludovicmuller1@gmail.com>
2018-08-08 15:16:26 +0200

    improved description parser

--- Parser/DescriptionParser.php
+++ Parser/DescriptionParser.php
@@ -35,6 +35,19 @@ class DescriptionParser extends Parser {
         }
 
         if (empty($this->results)) {
+            foreach ($xp->query("//meta[@name='DESCRIPTION']") as $el) {
+                $this->results = $this->cleanString($el->getAttribute("content"));
+                if (!empty($this->results)) break;
+            }
+        }
+
+        if (empty($this->results)) {
+            $data = preg_replace('#<script(.*?)>(.*?)</script>#is', '', $data);
+            $data = preg_replace('#<style(.*?)>(.*?)</style>#is', '', $data);
+            $data = preg_replace('#<a(.*?)>(.*?)</a>#is', '', $data);
+            $data = preg_replace('#<ul(.*?)>(.*?)</ul>#is', '', $data);
+            $data = preg_replace('#<ol(.*?)>(.*?)</ol>#is', '', $data);
+            $data = trim($data);
             $this->results = mb_strimwidth(
                 $this->cleanString($data), 0, 500, "..."
             );
